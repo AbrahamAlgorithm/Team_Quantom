9dc89f1bb5aec997055df121d25736e09216f2a2
izzyjosh <calebayuba222@gmail.com>
2024-09-22 16:37:23 +0100

    feat: added User model

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+from uuid import UUID
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    id: UUID = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    username: str = models.CharField(max_length=50)
+    email: str = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELD = []
+
+    def __str__(self) -> str:
+        return self.username
