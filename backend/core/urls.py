bd7bfee359975f0538ab1cb2f025267f192eb2cc
izzyjosh <calebayuba222@gmail.com>
2024-09-22 18:19:52 +0100

    feat: added docs for API

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# drf_yasg configurations
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Doctrim API",
+        default_version="v1",
+        description="This API provides authentication",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    url="swagger/",
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.v1.urls")),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
