e4069bc7b399057a6ed21ec48c7c751a6f4bc1fd
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-09-25 17:34:59 +0100

    feat: added homepage content

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-[#263238] mt-[3em] p-6">
+      <div className="">
+        Footer
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
